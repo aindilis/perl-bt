10c5fe3c3c412ec71522a47d260a2a4529066646
Andrew Dougherty <andrewdo@frdcsa.org>
2020-05-31 14:55:13 -0400

    Put btsk into https://github.com/aindilis/perl-btsk

--- btsk/transpiler.pl
+++
@@ -1,8 +0,0 @@
-:- use_module(library(transpiler)).
-:- set_prolog_flag(double_quotes,chars).
-:- initialization(main).
-
-main :-
-        translate("function add(a,b){return a + b;}",javascript,lua,X),
-        atom_chars(Y,X),
-        writeln(Y).
